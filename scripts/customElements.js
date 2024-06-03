5852e455a170dcf676bff8727c0ce0e6a208f91b
kengomatsuo <kennethfang1000@gmail.com>
2024-06-04 00:07:22 +0700

    Started on the register page and from

--- scripts/customElements.js
+++ scripts/customElements.js
@@ -1,14 +1,22 @@
+console.log(window.location.pathname)
+const currentPath = window.location.pathname
+const checkHome = (currentPath === '/index.html') ? 'id="current"' : ''
+const checkSubs = (currentPath === '/subscription/index.html') ? 'id="current"' : ''
+const checkRevi = (currentPath === '/testimony/index.html') ?'id="current"' : ''
+const checkLear = (currentPath === '/learning/index.html') ? 'id="current"' : ''
+const checkSign = (currentPath === '/register/index.html') ? 'hidden' : ''
+
 class customHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <header class="shaded-vertical">
         <a class="lusitana-regular logo">JA</a>
         <nav>
-          <a class="inter-regular navigation-button" href="/index.html">Home</a>
-          <a class="inter-regular navigation-button" href="/subscription/index.html">Products</a>
-          <a class="inter-regular navigation-button" href="/testimony/index.html">Reviews</a>
-          <a class="inter-regular navigation-button" href="/learning/index.html">Learn</a>
-          <a class="inter-regular black-button signup" href="/register/index.html">Sign up</a>
+          <a class="inter-regular navigation-button" ${checkHome} href="/index.html">Home</a>
+          <a class="inter-regular navigation-button" ${checkSubs} href="/subscription/index.html">Products</a>
+          <a class="inter-regular navigation-button" ${checkRevi} href="/testimony/index.html">Reviews</a>
+          <a class="inter-regular navigation-button" ${checkLear} href="/learning/index.html">Learn</a>
+          <a class="inter-regular black-button signup ${checkSign}" href="/register/index.html">Sign up</a>
         </nav>
       </header>
     `
